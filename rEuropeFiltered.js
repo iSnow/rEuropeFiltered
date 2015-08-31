a8c7e97865d8abef7ef7cd7a2a6325428353ded2
Johannes Jander <johannes@jandermail.de>
2015-08-31 16:54:47 +0200

    Working filter impl.

--- rEuropeFiltered.js
+++ rEuropeFiltered.js
@@ -17,6 +17,7 @@
 	//load only on overview page, not individual threads
 	if ((tBody != undefined) && (classes.indexOf('listing-page')>-1)) {
 		//localStorage.clear();
+		reuropeDumpFilters();
 		reuropeFilter();
 		var fContainer = document.createElement("div");
 		fContainer.style.border = "1px solid #DBDADA";
@@ -49,17 +50,14 @@
 		cHtml += 'style="position: fixed; left: 0; right: 0; top: 0; bottom: 0; ';
 		cHtml += 'background: rgba(0, 0, 0, 0.2); display: none;" ';
 		cHtml += '>';
-		cHtml += '<div id="reurope-dialog-inner" ';
-		cHtml += 'style="position: absolute; left: auto; right: auto; background: white; ';
-		cHtml += 'min-width: 30em; min-height: 20em; top: 10em; width: 30%; padding: 2em; ';
-		cHtml += 'border-radius: 6px; border: 3px solid #4553A0; background: #f5f5f5; " ';
-		cHtml += '>';
-		cHtml += '<h3>Add a new filter definition</h3>';
-		cHtml += '<table><tr><td>Filter name</td><td><input id="reurope-filtername" type="text"></td></tr>';
-		cHtml += '<tr><td>Filter value</td><td><input id="reurope-filterval" type="text"></td></tr>';
-		cHtml += '<tr><td>Filter color</td><td><input id="reurope-filtercol" type="text"></td></tr>';
+		cHtml += '<div id="reurope-dialog-inner" >';
+		cHtml += '<h3 style="margin-left: 6px">Add a new filter definition</h3>';
+		cHtml += '<table><tr><td>Filter name</td><td><input id="reurope-filtername" type="text"></td><td></td></tr>';
+		cHtml += '<tr><td>Filter words</td><td><input id="reurope-filterval" type="text"></td><td></td></tr>';
+		cHtml += '<tr><td>Filter action</td><td><select id="reurope-filteraction"><option value="remove">Remove</option></select></td><td></td></tr>';
+		cHtml += '<tr><td>Filter color</td><td><input id="reurope-filtercol" type="text"></td><td>optional</td></tr>';
 		cHtml += '</table>';
-		cHtml += '<button disabled id="reurope-addfilter">OK</button><button id="reurope-cancel">Cancel</button>';
+		cHtml += '<div style="text-align: right; width: 24em"><button id="reurope-cancel">Cancel</button><button disabled id="reurope-addfilter">OK</button></div>';
 		cHtml += '</div>';
 		return cHtml;
 	}
@@ -77,10 +75,12 @@ function reuropeShowFilterDiag() {
 
 function reuropeAddFilter() {
 	var filter = {};
+	filter.active = true;
+	filter.action = document.getElementById("reurope-filteraction").value;
 	filter.id = 'reurope-filter-'+new Date().getTime();
 	filter.name = document.getElementById("reurope-filtername").value;
-	//filter.color = document.getElementById("reurope-filtercol").value;
-	filter.color = reuropeGetFilterColor (document.getElementById("reurope-filtercol").value, reuropeGetFilterIds().length+1);
+	var color = document.getElementById("reurope-filtercol").value.replace(/#/, '');
+	filter.color = reuropeGetFilterColor (color, reuropeGetFilterIds().length+1);
 	filter.expression = document.getElementById("reurope-filterval").value;
 	reuropeAppendFilterToList(filter);
 	
@@ -93,7 +93,6 @@ function reuropeRestoreFilterList() {
 	var fIds = reuropeGetFilterIds();
 	for (var i = 0; i < fIds.length; i++) {
 		var filter = JSON.parse(localStorage.getItem(fIds[i]));
-		console.log (filter);
 		reuropeAppendFilterToList(filter, i);
 	}
 }
@@ -114,7 +113,10 @@ function reuropeAppendFilterToList(filter, numFilters) {
 }
 
 function reuropeRemoveFilter(event) {
-	var node = event.target.parentNode;
+	var node = event.target;
+	while (!node.id) {
+		node = node.parentNode;
+	}	
 	var filterId = node.id;
 	localStorage.removeItem(filterId);
 	node.parentNode.removeChild(node);
@@ -123,26 +125,74 @@ function reuropeRemoveFilter(event) {
 }
 
 function reuropeToggleFilterState(event) {
-	var node = event.target.parentNode;
-	var classes = node.className;	
-	if (classes.indexOf('active')>-1) {
-		node.className = node.className.replace(/\bactive\b/,'');
-	} else {
-		node.className = node.className+' active';
+	var node = event.target;
+	while (!node.id) {
+		node = node.parentNode;
 	}
+
+	var id = node.id;
+	var filter = JSON.parse(localStorage.getItem(id));
+	var screenNode = node.getElementsByClassName('screen')[0];
+	var classes = screenNode.className;	
+	if (classes.indexOf('active')>-1) {
+		filter.active = false;
+		screenNode.className = screenNode.className.replace(/\bactive\b/,'');
+	} else {
+		filter.active = true;
+		screenNode.className = screenNode.className+' active';
+	}
+
+	localStorage.setItem(filter.id, JSON.stringify(filter));
 	reuropeFilter();
 }
 
 function reuropeFilter() {
-	console.log ('reuropeFilter');
+	//reuropeGetThreads();
+
+	var allTerms = [];
+	var ids = reuropeGetFilterIds();
+	for (var i = 0; i < ids.length; i++) {
+		var filter = JSON.parse(localStorage.getItem(ids[i]));
+		if ((filter.active) && (filter.expression)) {
+			var terms = filter.expression.split(',');
+			for (var j = 0; j < terms.length; j++) {
+				allTerms.push (terms[j].trim());
+			}
+		} 
+	}
+	var threadList = document.getElementById('siteTable');
+	var threads = threadList.getElementsByClassName('entry');	
+	for (var i = 0; i < threads.length; i++) {
+		var topicWrap = threads[i].getElementsByClassName('title')[0];
+		var topic = topicWrap.getElementsByClassName('title')[0];	
+		var txt = topic.text;
+		for (var j = 0; j < allTerms.length; j++) {
+			if (topic.text.indexOf(allTerms[j]) > -1) {
+				if (filter.action == 'remove') {
+					threads[i].parentNode.style.display = 'none';
+				}
+				
+				break;
+			}
+		}
+	}
 }
 
 function reuropeCancel() {
-	console.log ('cancel');
 	var fDiag = document.getElementById("reurope-dialog");
 	fDiag.style.display = 'none';
 }
 
+function reuropeGetThreads() {
+	var topics = {};
+	var threadList = document.getElementById('siteTable');
+	var threads = threadList.getElementsByClassName('entry');	
+	for (var i = 0; i < threads.length; i++) {
+		var topicWrap = threads[i].getElementsByClassName('title')[0];
+		var topic = topicWrap.getElementsByClassName('title')[0];	
+		topics[threads[i].parentNode.getAttribute("data-fullname")] = topic.text;
+	}
+}
 
 function reuropeGetFilterIds() {
 	var ids = [];
@@ -155,9 +205,16 @@ function reuropeGetFilterIds() {
 	return ids;
 }
 
+function reuropeDumpFilters() {
+	var ids = reuropeGetFilterIds();
+	for (var i = 0; i < ids.length; i++) {
+		var filter = JSON.parse(localStorage.getItem(ids[i]));
+		console.log (filter);
+	}
+}
+
+
 function reuropeGetFilterColor (color, index) {
-	console.log ("color: "+color);
-	console.log ("index: "+index);
 	cols = reuropeColors();
 	if (!color) {
 		color = cols[0];
@@ -165,7 +222,6 @@ function reuropeGetFilterColor (color, index) {
 			color = cols[index];
 		}
 	}
-	console.log ("color: "+color);
 	return color;
 }
 
@@ -189,7 +245,7 @@ function injectStyles() {
 		style += "border-radius: 2px; \n";
 		style += "} \n";
 		style += ".reurope-filter>.screen {";
-		style += "background: rgba(0, 0, 0, 0.2); \n";
+		style += "background: #666; \n";
 		style += "padding: 8px 6px 6px 6px;\n";
 		style += "} \n";
 		style += ".reurope-filter>.screen.active {";
@@ -216,7 +272,38 @@ function injectStyles() {
 		style += "color: rgba(255, 255, 255, 0.3); \n";
 		style += "font-weight: 700 \n";
 		style += "} \n";
-		
+		style += "#reurope-dialog-inner { \n";
+		style += "background: #f5f5f5 none repeat scroll 0 0; \n";
+		style += "border: 3px solid #4553a0; \n";
+		style += "border-radius: 6px; \n";
+		style += "left: 50%; \n";
+		style += "margin-right: -50%; \n";
+		style += "height: 15em; \n";
+		style += "padding: 2em; \n";
+		style += "position: absolute; \n";
+		style += "right: auto; \n";
+		style += "top: 50%; \n";
+		style += "transform: translate(-50%, -50%); \n";
+		style += "width: 30em; \n";
+		style += "box-shadow: 0 0 6px rgba(49, 63, 200, 0.6);\n";
+		style += "} \n";
+		style += "#reurope-dialog input[type='text'] { \n";
+		style += "border: 1px solid #dbdada; \n";
+		style += "border-radius: 2px; \n";
+		style += "font-size: 14px; \n";
+		style += "padding: 4px; \n";
+		style += "width: 15em; \n";
+		style += "box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset; \n";
+		style += "transition: border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; \n";
+		style += "} \n";
+		style += "#reurope-dialog input[type='text']:focus { \n";
+		style += "border: 1px solid #4553a0; \n";
+		style += "box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 4px rgba(49, 63, 200, 0.15);\n";
+		style += "} \n";
+		style += "#reurope-dialog-inner>table td { \n";
+		style += "padding: 4px 6px 4px 6px; \n";
+		style += "} \n";
+
 		var sContainer = document.createElement("style");
 		sContainer.type = 'text/css';
 		sContainer.appendChild(document.createTextNode(style));
